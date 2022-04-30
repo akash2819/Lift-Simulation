68500f302f72008177fd2af8da1156cbc723293a
akash2819 <mr.robot191121@gmail.com>
2022-05-01 02:36:05 +0530

    created lift and floor for lift simulation

---
+++ main.js
@@ -0,0 +1,10 @@
+// function greet(){
+//     alert('Welcome to lift simulation!!');
+// }
+// // greet();
+// function button_manu(){
+//   const btn= document.querySelector('.btn');
+//   btn.addEventListener('click',()=>{alert("Boom!")});  
+// }
+
+// button_manu();
